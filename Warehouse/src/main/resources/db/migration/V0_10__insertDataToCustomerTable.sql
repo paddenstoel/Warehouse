650db90db5d96c0d2fada66992b66d32dcde450e
v.androsenko <v.androsenko@i-sys.ru>
2021-11-15 15:32:07 +0400

    row inserting to tr_customers with null address

--- Warehouse/src/main/resources/db/migration/V0_10__insertDataToCustomerTable.sql
+++ Warehouse/src/main/resources/db/migration/V0_10__insertDataToCustomerTable.sql
@@ -1,4 +1,5 @@
 insert into warehouse.tr_customers (id, last_name, first_name, customer_contact_number, customer_address)
 values ('32169ea2-51c1-4c9f-9c80-226a99a93b97', 'Wonka', 'Willy', '+4545454545', '813 Howard Street Oswego NY 13126'),
        ('3ab230a5-441d-4344-8133-f53c4ca39034', 'Heisenberg', 'Ed', '+654789521',
-        '4565 Coventry Court City Gulfport State/Province abbr MS State/Province full Mississippi Zip Code/Postal code 39501')
+        '4565 Coventry Court City Gulfport State/Province abbr MS State/Province full Mississippi Zip Code/Postal code 39501'),
+        ('5e5c2741-012a-48e1-adc6-1633c8b095b9', 'Zillibobka', 'Zilly', '+88989858', null)
